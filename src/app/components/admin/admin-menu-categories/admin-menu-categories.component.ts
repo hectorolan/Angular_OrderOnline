5cda8d08d4276bd3474bbaa49b3e10f3a1e481f4
Hector Olan <olanhector@gmail.com>
2021-03-06 12:16:34 -0400

    update UI for AdminCategories

--- src/app/components/admin/admin-menu-categories/admin-menu-categories.component.ts
+++ src/app/components/admin/admin-menu-categories/admin-menu-categories.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-menu-categories.component.css']
 })
 export class AdminMenuCategoriesComponent implements OnInit {
+    typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  savePage() {
+  }
+
+  resetPage() { 
+
   }
 
 }
